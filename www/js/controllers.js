bb0622c9bceeb93c9d3d62c5bdbffdd92de6c371
Bruce Grover Lee <brucegrover.lee@gmail.com>
2016-04-01 22:11:33 -0500

    now has camera

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -12,7 +12,20 @@ angular.module('starter.controllers', [])
   }
 })
 
-.controller('ChatCtrl', function ($scope, $ionicScrollDelegate, $cordovaVibration, socket) {
+.controller('ChatCtrl', function ($scope, $ionicScrollDelegate, $cordovaVibration, $ionicModal, socket, Camera) {
+
+  $ionicModal.fromTemplateUrl('templates/camera.html', {
+    scope: $scope
+  }).then(function(modal) {
+    $scope.modal = modal
+  })
+
+  $scope.openModal = function() {
+    $scope.modal.show()
+  }
+  $scope.closeModal = function() {
+    $scope.modal.hide()
+  }
 
   var user = localStorage.getItem('userChat')
   user = JSON.parse(user)
@@ -39,6 +52,21 @@ angular.module('starter.controllers', [])
     $ionicScrollDelegate.$getByHandle('scroll').scrollBottom()
     socket.emit('new message', $scope.comment.text)
     $scope.comment.text = null
+  }
+
+  $scope.getPhoto = function() {
+    console.log('Getting camera');
+    Camera.getPicture({
+      quality: 75,
+      targetWidth: 320,
+      targetHeight: 320,
+      saveToPhotoAlbum: false
+    }).then(function(imageURI) {
+      console.log(imageURI);
+      $scope.lastPhoto = imageURI;
+    }, function(err) {
+      console.err(err);
+    });
   }
 
 })
--- www/js/services.js
+++ www/js/services.js
@@ -50,3 +50,20 @@ angular.module('starter.services', [])
 
 return mySocket;
 })
+
+.factory('Camera', function($q) {
+  return {
+    getPicture: function(options) {
+      var q = $q.defer()
+
+      navigator.camera.getPicture(function(result) {
+        // Do any magic you need
+        q.resolve(result)
+      }, function(err) {
+        q.reject(err)
+      }, options)
+
+      return q.promise;
+    }
+  }
+})
